3d6c4b3d1921ac91cbf7c50de2cc040aad17dc79
Patrick Hogan <pmhogan@gmail.com>
2013-08-30 00:44:04 +0200

    Data import recoded and output rds file check against original

--- data-count.r
+++ data-count.r
@@ -1,20 +1,12 @@
 source("header.r")
 
-db <- odbcConnectExcel2007("Kettle River data.xlsx")
-
-data <- sqlQuery(db, paste(
-  "SELECT * ",
-  "FROM \"Sheet1$\" ",
-  sep=""))
-
-odbcCloseAll()
-
+# Data import, selection and renaming
+data <- read.csv("Kettle River data.csv")
 data <- subset(data,Replicate == 1)
+data <- subset(data,select = c("Year","Site","SiteLength..km.","Marked","MarkObserved","Unmarked.Observed"))
+colnames(data) <- c("Year","Site","SiteLength","Mark","Recap","Total") # Final column isn't yet the total; will become so below
 
-data <- subset(data,select = c("Year","Site","SiteLength (km)","Marked","MarkObserved","Unmarked Observed"))
-
-colnames(data) <- c("Year","Site","SiteLength","Mark","Recap","Total")
-
+# Data correction, cleaning and shaping
 warning("corrects 2010 data at GR3 so total count 242 not 86")
 data$Total[data$Site == "GR3" & data$Year == 2010 & data$Total == 86]<-242
 
@@ -28,18 +20,18 @@ data$Site <- factor(data$Site)
 data$Year <- factor(data$Year)
 data$Mark[is.na(data$Mark)] <- 0
 data$Recap[is.na(data$Recap)] <- 0
-data$Total <- data$Recap + data$Total
+data$Total <- data$Recap + data$Total #Turns final column into total
 
 data$Mark <- as.integer(data$Mark)
 data$Recap <- as.integer(data$Recap)
 data$Total <- as.integer(data$Total)
 
 warning("this is a hack to deal with inconsistent marks and recaps")
-
 mark <- pmax(data$Mark, data$Recap)
 recap <- pmin(data$Mark, data$Recap)
-
 data$Mark <- mark
 data$Recap <- recap
 
-saveRDS(data,"count.rds")
+# Save data to disk
+set_folders("count")
+save_rdata(data)
