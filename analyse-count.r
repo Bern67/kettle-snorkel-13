7c5f76865e754b470bdd15549ac065315227fb19
Joe Thorley <joe@poissonconsulting.ca>
2014-01-06 14:13:32 -0600

    only save_plots and plot_residuals if not in debug mode

--- analyse-count.r
+++ analyse-count.r
@@ -1,15 +1,19 @@
 source("header.r")
-source("model-count.r")
+source("models-count.r")
 
-# Load data from disk
 set_folders("count")
-count <- load_rdata()
 
-# Run, save and sumarise jagganaut analysis
-analysis <- jags_analysis(models, data = count, niter = 10^5)   
+data <- load_rdata()
+
+analysis <- jags_analysis(models, data = data, niters = 10^5)
 
 save_analysis(analysis)
+
 print(summary(analysis))
 
 save_tables(analysis)
-save_plots(analysis)
+
+if (opts_jagr("mode") != "debug") {
+  save_plots(analysis)
+  plot_residuals(analysis)
+}
