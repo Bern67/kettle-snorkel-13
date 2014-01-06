369e0f9e46d0969286a072e41d4240dbb3fbef14
Joe Thorley <joe@poissonconsulting.ca>
2014-01-06 15:34:35 -0600

    pandoc at end in case not installed

--- knit-files.r
+++ knit-files.r
@@ -1,9 +1,10 @@
 source("header.r")
 
 knit2html("methods.rmd")
-system("pandoc -s methods.md -o methods.docx")
 knit2html("models.rmd")
-system("pandoc -s models.md -o models.docx")
 knit2html("parameters.rmd")
-system("pandoc -s parameters.md -o results.docx")
 knit2html("figures.rmd") 
+
+system("pandoc -s methods.md -o methods.docx")
+system("pandoc -s models.md -o models.docx")
+system("pandoc -s parameters.md -o results.docx")
