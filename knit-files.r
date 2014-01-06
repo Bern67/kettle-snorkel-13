4fd639e17c4d1ff1f969963dbb1e9659c144deff
Joe Thorley <joe@poissonconsulting.ca>
2014-01-06 17:21:42 -0600

    combined figures, methods, models and parameters into report

--- knit-files.r
+++ knit-files.r
@@ -1,10 +1,4 @@
 source("header.r")
 
-knit2html("methods.rmd")
-knit2html("models.rmd")
-knit2html("parameters.rmd")
-knit2html("figures.rmd") 
-
-system("pandoc -s methods.md -o methods.docx")
-system("pandoc -s models.md -o models.docx")
-system("pandoc -s parameters.md -o results.docx")
+knit2html("report.rmd") 
+system("pandoc -s report.md -o report.docx")
