d2c5c4aec263879bb6c2ad7e989351efe0e75c00
Joe Thorley <joe@poissonconsulting.ca>
2014-01-06 17:35:58 -0600

    removoed second order discharge effect

--- models-count.r
+++ models-count.r
@@ -1,30 +1,29 @@
 description = c(
-  "bEfficiency0" = "Logit observer efficiency intercept",
-  "bDensity0" = "Log density intercept",
-  "sDensitySite" = "SD of effect of site on log density",
-  "nSite" = "Number of sites",
-  "bDensitySite[st]" = "Effect of stth site on log density",
-  "sDensityYear" = "SD of effect of year on log density",
-  "nYear" = "Number of years",
-  "sDensitySiteYear" = "SD of effect of site in year on log density",
-  "bDensityYear[yr]" = "Effect of yrth year on log density",
-  "bDensitySiteYear[st, yr]" = "Effect of stth site in yrth year on log density",
-  "nrow" = "Number of site counts",
-  "eEfficiency[i]" = "Expected observer efficiency on the ith site count",
-  "Released[i]" = "Number of fish marked prior to the ith site count",                  
-  "Resighted[i]" = "Number of marked fish observed during ith site count",
-  "eDensity[i]" = "Expected density at ith site count",
-  "SiteLength[i]" = "Length site during ith site count",
-  "eAbundance[i]" = "Expected abundance at ith site count",
-  "eCount[i]" = "Expected total number of fish at ith site count",
-  "Count[i]" = "Total number of fish observed during ith site count"
+  "`bEfficiency0`" = "Log odds observer efficiency intercept",
+  "`bEfficiencyDischarge`" = "Effect of standardised discharge on `bEfficiency0`",
+  "`bDensity0`" = "Log lineal fish density intercept",
+  "`sDensitySite`" = "SD of effect of site on `bDensity0`",
+  "`bDensitySite[st]`" = "Effect of st^th site on `bDensity0`",
+  "`sDensitySiteYear`" = "SD of effect of site in year on `bDensity0`",
+  "`bDensitySiteYear[st, yr]`" = "Effect of st^th site in yr^th year on `bDensity0`",
+  "`sDensityYear`" = "SD of effect of year on `bDensity0`",
+  "`bDensityYear[yr]`" = "Effect of yr^th year on `bDensity0`",
+  "`Discharge[i]`" = "Discharge during i^th site visit",
+  "`eEfficiency[i]`" = "Expected observer efficiency during the i^th site visit",
+  "`ProportionSurveyed[i]`" = "Proportion of site surveyed during i^th site visit",                  
+  "`Released[i]`" = "Number of fish marked prior to the i^th site visit",                  
+  "`Resighted[i]`" = "Number of marked fish observed during i^th site visit",
+  "`eDensity[i]`" = "Expected lineal fish density during i^th site visit",
+  "`SiteLength[i]`" = "Length of site during i^th site visit",
+  "`eAbundance[i]`" = "Expected abundance during i^th site visit",
+  "`eCount[i]`" = "Expected total number of fish observed during i^th site visit",
+  "`Count[i]`" = "Total number of fish observed during i^th site visit"
 )
 
 model1 <- jags_model("model {
 
   bEfficiency0 ~ dnorm(0, 2^-2)
   bEfficiencyDischarge ~ dnorm(0, 2^-2)
-  bEfficiencyDischarge2 ~ dnorm(0, 2^-2)
 
   bDensity0 ~ dnorm(0, 5^-2)
 
@@ -43,9 +42,7 @@ model1 <- jags_model("model {
   }         
                    
   for (i in 1:length(Count)) {
-    logit(eEfficiency[i]) <- bEfficiency0 + bEfficiencyDischarge * Discharge[i] +
-                              bEfficiencyDischarge2 * Discharge[i]^2
-  
+    logit(eEfficiency[i]) <- bEfficiency0 + bEfficiencyDischarge * Discharge[i]  
     dEfficiency[i] <- min(eEfficiency[i], Released[i])
     dReleased[i] <- max(Released[i], 1)
     Resighted[i] ~ dbin(dEfficiency[i] * ProportionSurveyed[i], dReleased[i])
@@ -62,9 +59,7 @@ model1 <- jags_model("model {
 derived_code = "model {
 
   for (i in 1:length(Count)) {
-    logit(eEfficiency[i]) <- bEfficiency0 + bEfficiencyDischarge * Discharge[i] +
-                              bEfficiencyDischarge2 * Discharge[i]^2
-  
+    logit(eEfficiency[i]) <- bEfficiency0 + bEfficiencyDischarge * Discharge[i]  
     log(prediction[i]) <- bDensity0 + bDensityYear[Year[i]]
                                   + bDensitySite[Site[i]]
                                   + bDensitySiteYear[Site[i], Year[i]]
