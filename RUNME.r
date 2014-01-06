93cd0b2573cc1a6c9c96f09fdaf160a2e1626dc8
Joe Thorley <joe@poissonconsulting.ca>
2014-01-06 13:16:47 -0600

    replaced project.r with RUNME.r

---
+++ RUNME.r
@@ -0,0 +1,9 @@
+source("install-packages.r")
+
+source("input-data.r")
+
+source("manipulate-count.r")
+source("analyse-count.r")
+source("plot-count.r")
+
+source("knit-files.r")
--- project.r
+++
@@ -1,5 +0,0 @@
-rm(list=ls()) # Clear out memory
-
-source("data-count.r")
-source("analysis-count.r")
-source("plot-count.r")
