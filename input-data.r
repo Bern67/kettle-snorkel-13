b36fb94168c9bb0520ca2a2dd43ebc7bf8077be2
Joe Thorley <joe@poissonconsulting.ca>
2014-01-06 14:13:49 -0600

    new input-data script to read from access database

---
+++ input-data.r
@@ -0,0 +1,39 @@
+source("header.r")
+
+if (.Platform$OS.type != "windows") {
+  stop("data-input must be run on windows as it queries an Access database")
+}
+
+set_folders("input")
+
+db <- odbcConnectAccess2007("input/WKE_KET_GRASwimCounts.accdb")
+
+data <- sqlQuery(db, "SELECT *  FROM XqryAllSectionFishAnalysisFinal")
+
+odbcCloseAll()
+
+data <- rename(data, replace = c("PrimarySystem" = "System"))
+data <- rename(data, replace = c("Section" = "Site"))
+data <- rename(data, replace = c("GNIS_NAME" = "River"))
+data <- rename(data, replace = c("MaxSectionKm" = "SiteLength"))
+data <- rename(data, replace = c("PercentSuvey" = "ProportionSurveyed"))
+data <- rename(data, replace = c("CaptureMark" = "Released"))
+data <- rename(data, replace = c("T-Bar" = "Resighted"))
+data <- rename(data, replace = c("OldT-Bar" = "OldResighted"))
+data <- rename(data, replace = c("UnMarked" = "Unmarked"))
+
+data$Date <- as.Date(data$Date)
+is.na(data$Visibility[data$Visibility < 0]) <- TRUE
+warning("visibility of -1")
+
+message("need riverkm for site order")
+
+data$Released[is.na(data$Released)] <- 0
+data$OldResighted[is.na(data$OldResighted)] <- 0
+data$Resighted[is.na(data$Resighted)] <- 0
+
+data$Count <- data$Unmarked + data$OldResighted + data$Resighted
+data$OldResighted <- NULL
+data$Unmarked <- NULL
+
+save_rdata(data)
