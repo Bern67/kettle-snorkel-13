f15ac715ac14a3c854045c734347a3a69167c321
Joe Thorley <joe@poissonconsulting.ca>
2014-01-06 15:19:01 -0600

    plot efficiency by discharge

--- plot-count.r
+++ plot-count.r
@@ -48,12 +48,13 @@ print(gp)
 
 save_plot("density-site-year")
 
-prediction <- predict(analysis, newdata = c("Year"), parm = "eEfficiency")
-prediction$Year <- as.integer(as.character(prediction$Year))
+prediction <- predict(analysis, newdata = c("Discharge"), parm = "eEfficiency")
 
-gp <- ggplot(data = prediction, aes(x = Year, y = estimate))
-gp <- gp + geom_pointrange(aes(ymin = lower, ymax = upper))
-gp <- gp + scale_x_continuous(name = "Year", breaks = c(2000,2005,2010))
+gp <- ggplot(data = prediction, aes(x = Discharge, y = estimate))
+gp <- gp + geom_line()
+gp <- gp + geom_line(aes(y = lower), linetype = "dotted")
+gp <- gp + geom_line(aes(y = upper), linetype = "dotted")
+gp <- gp + scale_x_continuous(name = "Discharge (cms)")
 gp <- gp + scale_y_continuous(name = "Observer Efficiency", labels = percent, 
                               expand = c(0,0))
 gp <- gp + expand_limits(y = c(0,1))
